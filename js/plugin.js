b40402c07100f4511a29bcdded2866d2d8b73a25
Aaron Griffis <aarongriffis@hotmail.com>
2017-10-31 14:11:45 -0700

    Add work from Codepen

--- js/plugin.js
+++
@@ -1,1 +0,0 @@
-// Paste jQuery Plugin Code Here
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-// Store 3 images
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
+$(document).ready(loaded);
 
-// Get random number
-var max = 3;
-var numRand = Math.floor( Math.random() * max );
-console.log(numRand);
-
-// Display randomized images
-$('#random').attr( 'src', eval('img' + numRand) );
-
+function loaded() {
+  var leaves = 40;
+  
+  for(var i = 0; i < leaves; i++) {
+    $('<svg xmlns="http://www.w3.org/2000/svg" class="leaf" width="3" height="3" viewBox="-1 -1 6 6"><path d="M0 0 v 1 a 2 2 0 0 0 2 2 h 1 v -1 a 2 2 0 0 0 -2 -2 z M1 1 l 2.5 2.5 M2 1 v 1"/></svg>').appendTo('#leaves');
+  }
+}
